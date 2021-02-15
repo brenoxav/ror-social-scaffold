2eb3d00231457e41372b9d3b7a3b136c8218cb66
Breno Xavier <brenoxav@gmail.com>
2021-02-15 12:57:48 -0400

    Implement friendships controller, 'add_friend-btn' button and helper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,32 +10,37 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :submitted_friendships, foreign_key: 'submitter_id', class_name: 'Friendships'
-  has_many :received_friendships, foreign_key: 'receiver_id', class_name: 'Friendships'
+  has_many :submitted_friendships, foreign_key: 'submitter_id', class_name: 'Friendship'
+  has_many :received_friendships, foreign_key: 'receiver_id', class_name: 'Friendship'
 
-  #has_many :submitted_friendships, ->(user) { where(submitter_id: :user_id) }, foreign_key: 'submitter_id', class_name: 'Friendships'
-  #has_many :received_friendships, ->(user) { where(receiver_id: :user_id) }, foreign_key: 'submitter_id', class_name: 'Friendships'
-  #has_many :friendships, -> { where("submitter_id = ? OR receiver_id = ?", id, id) }
-  #has_many :friendships
-
+  # Return an array with all confirmed friends (submitted and received)
   def friends
-    Friendship.where(submitter_id: id, status: true).pluck(:receiver_id) + Friendship.where(receiver_id: id, status: true).pluck(:submitter_id)
+    s = submitted_friendships.map { |f| f.receiver if f.status }
+    r = received_friendships.map { |f| f.submitter if f.status }
+    (s + r).compact
   end
 
-  def submit_friendship(receiver_id)
-    Friendship.create(submitter_id: self.id, receiver_id: receiver_id)
+  def friendships
+    s = submitted_friendships
+    r = received_friendships
+    (s + r)
   end
 
-  def abort_friendship(receiver_id)
-    Friendship.where(submitter_id: self.id).and(Friendship.where(receiver_id: receiver_id)).destroy
+  def pending_submissions
+    submitted_friendships.select { |f| f.submitter if !f.status }
   end
 
-  def accept_friendship(submitter_id)
-    Friendship.where(submitter_id: submitter_id).and(Friendship.where(receiver_id: self.id)).update(status: true)
+  def pending_receipts
+    received_friendships.select { |f| f.receiver if !f.status }
   end
 
-  def reject_friendship(submitter_id)
-    Friendship.where(submitter_id: submitter_id).and(Friendship.where(receiver_id: self.id)).destroy
+  def confirm_friend(submitter)
+    friendship = received_friendships.find{ |f| f.submitter == submitter }
+    friendship.status = true
+    friendship.save
   end
 
+  def friend?(user)
+    friends.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :friendships, only: [:create, :update, :destroy]
+
   root 'posts#index'
 
   devise_for :users
@@ -10,6 +12,6 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
 
-  resources :friendships, only: [:create, :update, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
